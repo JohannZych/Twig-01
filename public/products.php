4a7b633406eb2f9b46f8ec4214ad1dba8b0616fd
zych.johann@gmail.com <zych.johann@gmail.com>
2022-10-14 16:49:19 +0200

    First step Twig

---
+++ public/products.php
@@ -0,0 +1,13 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+/*$products = 'Produits';*/
+echo $twig->render('products.html.twig', ['products' => $products]);
+
+
+
